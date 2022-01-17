95041a68cdd65f66906512859463cb886252120b
So Yeon Min <soyeonmin@Soui-MacBookPro.local>
2022-01-17 17:48:16 -0500

    ood pre

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -142,8 +142,8 @@ def get_dataset(P, dataset, test_only=False, image_size=None, download=False, ev
         #train_set = datasets.CIFAR10(DATA_PATH, train=True, download=download, transform=train_transform)
         #test_set = datasets.CIFAR10(DATA_PATH, train=False, download=download, transform=test_transform)
         test_dir = os.path.join(DATA_PATH, 'co3d_temp/temp_train_224_just_two_each')
-        train_dir = os.path.join(DATA_PATH, 'imagnet_small')
-        train_set = datasets.ImageFolder(train_dir, transform=train_transform)
+        #train_dir = os.path.join(DATA_PATH, 'imagnet_small')
+        #train_set = datasets.ImageFolder(train_dir, transform=train_transform)
         test_set = datasets.ImageFolder(test_dir, transform=test_transform)
 
 
