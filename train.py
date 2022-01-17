edc2ed4e824867df2531c873aeb5f156ec0f162b
So Yeon Min <soyeonmin@Soui-MacBookPro.local>
2022-01-17 17:39:13 -0500

    ood pre

--- train.py
+++ train.py
@@ -46,7 +46,38 @@ for epoch in range(start_epoch, P.epochs + 1):
 		#    save_states = model.state_dict()
 		#save_checkpoint(epoch, save_states, optimizer.state_dict(), logger.logdir)
 		#save_linear_checkpoint(linear_optim.state_dict(), logger.logdir)
-		pass
+		#pass
+
+		from evals.ood_pre import eval_ood_detection
+
+	    with torch.no_grad():
+	        auroc_dict = eval_ood_detection(P, model, test_loader, ood_test_loader, P.ood_score,
+	                                        train_loader=train_loader, simclr_aug=simclr_aug)
+
+	    if P.one_class_idx is not None:
+	        mean_dict = dict()
+	        for ood_score in P.ood_score:
+	            mean = 0
+	            for ood in auroc_dict.keys():
+	                mean += auroc_dict[ood][ood_score]
+	            mean_dict[ood_score] = mean / len(auroc_dict.keys())
+	        auroc_dict['one_class_mean'] = mean_dict
+
+	    bests = []
+	    for ood in auroc_dict.keys():
+	        message = ''
+	        best_auroc = 0
+	        for ood_score, auroc in auroc_dict[ood].items():
+	            message += '[%s %s %.4f] ' % (ood, ood_score, auroc)
+	            if auroc > best_auroc:
+	                best_auroc = auroc
+	        message += '[%s %s %.4f] ' % (ood, 'best', best_auroc)
+	        if P.print_score:
+	            print(message)
+	        bests.append(best_auroc)
+
+	    bests = map('{:.4f}'.format, bests)
+	    print('\t'.join(bests))
 
 	if epoch % P.error_step == 0 and ('sup' in P.mode):
 		error = test_classifier(P, model, test_loader, epoch, logger=logger)
