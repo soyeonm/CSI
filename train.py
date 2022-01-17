6d320ba4663c11bae0815db3e4c2c386aac88ae5
So Yeon Min <soyeonmin@Soui-MacBookPro.local>
2022-01-17 17:54:32 -0500

    ood pre

--- train.py
+++ train.py
@@ -50,13 +50,13 @@ for epoch in range(start_epoch, P.epochs + 1):
 
 		if P.multi_gpu:
 				save_states = model.module.state_dict()
-			else:
-				save_states = model.state_dict()
-			print("Saved at epoch ", epoch)
-			#save_checkpoint(epoch, save_states, optimizer.state_dict(), logger.logdir)
-			#save_linear_checkpoint(linear_optim.state_dict(), logger.logdir)
-			save_checkpoint(epoch, save_states, optimizer.state_dict(), 'ttemp_models')
-			save_linear_checkpoint(linear_optim.state_dict(), 'ttemp_models')
+		else:
+			save_states = model.state_dict()
+		print("Saved at epoch ", epoch)
+		#save_checkpoint(epoch, save_states, optimizer.state_dict(), logger.logdir)
+		#save_linear_checkpoint(linear_optim.state_dict(), logger.logdir)
+		save_checkpoint(epoch, save_states, optimizer.state_dict(), 'ttemp_models')
+		save_linear_checkpoint(linear_optim.state_dict(), 'ttemp_models')
 
 
 		from evals.ood_pre import eval_ood_detection
