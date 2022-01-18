049dcf7f76d80eedb695f2ba6d2a68f07a6bc281
So Yeon Min <soyeonmin@Soui-MacBookPro.local>
2022-01-18 06:04:08 -0500

    imagnet_small as ood for co3d

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -137,6 +137,7 @@ def get_dataset(P, dataset, test_only=False, image_size=None, download=False, ev
         train_transform, test_transform = get_transform(image_size=image_size)
 
     if dataset == 'co3d_small':
+        print("GOING THRU!")
         #test_only = True
         image_size = (224, 224, 3) #Just resize to this, so that we can get pretrained imagenet weights
         n_classes = 3
