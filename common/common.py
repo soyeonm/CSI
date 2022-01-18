2deeb715e3bd68d36224ccbb66c4057dd4c1418c
So Yeon Min <soyeonmin@Soui-MacBookPro.local>
2022-01-18 17:42:04 -0500

    co3d small split one

--- common/common.py
+++ common/common.py
@@ -6,7 +6,7 @@ def parse_args(default=False):
 
     parser = ArgumentParser(description='Pytorch implementation of CSI')
 
-    parser.add_argument('--dn', type=string, required=True)
+    parser.add_argument('--dn', type='string', required=True)
     parser.add_argument('--print_batch_size', help='debugging', action='store_true')
     parser.add_argument('--resize_cifar', help='debugging', action='store_true')
 
