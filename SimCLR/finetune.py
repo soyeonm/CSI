56e6eb30ea135be550826f2f7351dccd34972c24
So Yeon Min <soyeonmin@Soui-MacBookPro.local>
2022-01-24 15:20:48 -0500

    added finetune pysimclr

--- SimCLR/finetune.py
+++ SimCLR/finetune.py
@@ -12,6 +12,13 @@ from torchvision import datasets
 
 import argparse
 
+from torchvision import models
+
+
+model_names = sorted(name for name in models.__dict__
+                     if name.islower() and not name.startswith("__")
+                     and callable(models.__dict__[name]))
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("Using device:", device)
 
