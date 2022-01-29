b5096bcb2b1ade399306d2aebb0218a84d2f49b4
So Yeon Min <soyeonmin@soui-macbookpro.wifi.local.cmu.edu>
2022-01-29 17:45:50 -0500

    a

--- SimCLR/permclr.py
+++ SimCLR/permclr.py
@@ -114,100 +114,100 @@ class PermCLR(object):
 				batch_object_labels = batch_object_labels.to(self.args.device)
 				batch_category_labels = batch_category_labels.to(self.args.device)
 
-				pickle.dump(batch_imgs, open("batch_imgs.p", "wb"))
-				pickle.dump(batch_object_labels, open("batch_object_labels.p", "wb"))
-				pickle.dump(batch_category_labels, open("batch_category_labels.p", "wb"))
-				pickle.dump(self.args, open("args.p", "wb"))
+				#pickle.dump(batch_imgs, open("batch_imgs.p", "wb"))
+				#pickle.dump(batch_object_labels, open("batch_object_labels.p", "wb"))
+				#pickle.dump(batch_category_labels, open("batch_category_labels.p", "wb"))
+				#pickle.dump(self.args, open("args.p", "wb"))
 				#PART1
 				#1. Put all of the images into a model and get features
-				with torch.autograd.set_detect_anomaly(True):
-					with autocast(enabled=self.args.fp16_precision):
-						features = self.model(batch_imgs) #Shape should be like torch.Size([24, 128])
-						print("gpu memory after features: ", get_gpu_memory())
-						pickle.dump(features, open("features.p", "wb"))
+				#with torch.autograd.set_detect_anomaly(True):
+				with autocast(enabled=self.args.fp16_precision):
+					features = self.model(batch_imgs) #Shape should be like torch.Size([24, 128])
+					#print("gpu memory after features: ", get_gpu_memory())
+					#pickle.dump(features, open("features.p", "wb"))
 
-					#2. Rearrange these features (A) #M=  batch_size * num_categories (e.g. 6 in this case where there are 3 classes)
-						#features = features.reshape(self.args.batch_size * len(self.args.classes_to_idx), self.args.permclr_views, -1)
-						#TODO: check if this is correct
-						#Copy this matrix diagonally and apply it to batch_object_labels
-						A_mat = get_A_matrix(self.args.permclr_views) #8*8
-						A_mat = torch.block_diag(*[A_mat]*len(self.args.classes_to_idx)).to(self.args.device) #24x24 with Car1, Car2, Cat1, Cat2, ...
-						features = torch.mm(A_mat, features) #Now we are ready to reshape this and make "A". Reshaping this is "A".
-						pickle.dump(A_mat, open("A_mat1.p", "wb"))
-						pickle.dump(features, open("features1.p", "wb"))
-						batch_category_labels = torch.mm(A_mat, batch_category_labels.view(1,-1).T.float()).long()
-						batch_object_labels = torch.mm(A_mat, batch_object_labels.view(1,-1).T.float()).long()
+				#2. Rearrange these features (A) #M=  batch_size * num_categories (e.g. 6 in this case where there are 3 classes)
+					#features = features.reshape(self.args.batch_size * len(self.args.classes_to_idx), self.args.permclr_views, -1)
+					#TODO: check if this is correct
+					#Copy this matrix diagonally and apply it to batch_object_labels
+					A_mat = get_A_matrix(self.args.permclr_views) #8*8
+					A_mat = torch.block_diag(*[A_mat]*len(self.args.classes_to_idx)).to(self.args.device) #24x24 with Car1, Car2, Cat1, Cat2, ...
+					features = torch.mm(A_mat, features) #Now we are ready to reshape this and make "A". Reshaping this is "A".
+					#pickle.dump(A_mat, open("A_mat1.p", "wb"))
+					#pickle.dump(features, open("features1.p", "wb"))
+					batch_category_labels = torch.mm(A_mat, batch_category_labels.view(1,-1).T.float()).long()
+					batch_object_labels = torch.mm(A_mat, batch_object_labels.view(1,-1).T.float()).long()
 
-						features =features.reshape(self.args.batch_size*len(self.args.classes_to_idx), self.args.permclr_views, -1)  #THIS is A
-						batch_category_labels = batch_category_labels.squeeze().reshape(self.args.batch_size*len(self.args.classes_to_idx), self.args.permclr_views)
-						batch_object_labels = batch_object_labels.squeeze().reshape(self.args.batch_size*len(self.args.classes_to_idx), self.args.permclr_views)
-						print("gpu memory after A: ", get_gpu_memory())
-						
-					#3. Concatenate (B)
-						M = self.args.batch_size*len(self.args.classes_to_idx)
-						#features = torch.cat([torch.cat([features]*6, axis = 1).reshape(36,4,-1), torch.cat([features]*6)], axis=1)
-						features = torch.cat([torch.cat([features]*M, axis = 1).reshape(M**2,self.args.permclr_views,-1), torch.cat([features]*M)], axis=1)
-						batch_category_labels = torch.cat([torch.cat([batch_category_labels]*M, axis = 1).reshape(M**2,self.args.permclr_views), torch.cat([batch_category_labels]*M)], axis=1)
-						batch_object_labels = torch.cat([torch.cat([batch_object_labels]*M, axis = 1).reshape(M**2,self.args.permclr_views), torch.cat([batch_object_labels]*M)], axis=1)
-						print("gpu memory after B: ", get_gpu_memory())
+					features =features.reshape(self.args.batch_size*len(self.args.classes_to_idx), self.args.permclr_views, -1)  #THIS is A
+					batch_category_labels = batch_category_labels.squeeze().reshape(self.args.batch_size*len(self.args.classes_to_idx), self.args.permclr_views)
+					batch_object_labels = batch_object_labels.squeeze().reshape(self.args.batch_size*len(self.args.classes_to_idx), self.args.permclr_views)
+					#print("gpu memory after A: ", get_gpu_memory())
+					
+				#3. Concatenate (B)
+					M = self.args.batch_size*len(self.args.classes_to_idx)
+					#features = torch.cat([torch.cat([features]*6, axis = 1).reshape(36,4,-1), torch.cat([features]*6)], axis=1)
+					features = torch.cat([torch.cat([features]*M, axis = 1).reshape(M**2,self.args.permclr_views,-1), torch.cat([features]*M)], axis=1)
+					batch_category_labels = torch.cat([torch.cat([batch_category_labels]*M, axis = 1).reshape(M**2,self.args.permclr_views), torch.cat([batch_category_labels]*M)], axis=1)
+					batch_object_labels = torch.cat([torch.cat([batch_object_labels]*M, axis = 1).reshape(M**2,self.args.permclr_views), torch.cat([batch_object_labels]*M)], axis=1)
+					#print("gpu memory after B: ", get_gpu_memory())
 
-					#4. Permute (B P^T)
-						#Multiply by a permutation matrix for each row
-						#P_mat = torch.zeros(self.args.permclr_views*self.args.batch_size, self.args.permclr_views*self.args.batch_size).cuda()
-						P_mat = get_perm_matrix(self.args.permclr_views).to(self.args.device) #has shape 8x8 
-						#torch.cat([torch.cat([batch_category_labels]*6, axis = 1).reshape(-1,4), torch.cat([batch_category_labels]*6)], axis=1).shape
-						batch_category_labels = torch.mm(batch_category_labels.float(),P_mat.T) #Shape is 36x8
-						batch_object_labels = torch.mm(batch_object_labels.float(),P_mat.T)
-						#Apply bmm
-						#torch.cuda.empty_cache()
-						P_mat_128 = torch.cat([P_mat.unsqueeze(0)]*128, axis=0).float()
-						#P_mat_128 = P_mat_128.to(torch.device("cuda:1")) #Now shape is 128x8x8
-						pickle.dump(P_mat_128, open("P_mat_128.p", "wb"))
-						features = features.permute(2, 1, 0) #Now shape is 128 x 8x 36 (used to be 36 x 8x 128)
-						pickle.dump(features, open("features4.p", "wb"))
-						features = torch.bmm(P_mat_128, features) #shape is 128, 8, 36 
-						features = features.permute(0, 2, 1) #Shape is now 128 x 36 x 8. THIS IS (kind of? reshaped) THE PERMUTED B (B * P^T)
+				#4. Permute (B P^T)
+					#Multiply by a permutation matrix for each row
+					#P_mat = torch.zeros(self.args.permclr_views*self.args.batch_size, self.args.permclr_views*self.args.batch_size).cuda()
+					P_mat = get_perm_matrix(self.args.permclr_views).to(self.args.device) #has shape 8x8 
+					#torch.cat([torch.cat([batch_category_labels]*6, axis = 1).reshape(-1,4), torch.cat([batch_category_labels]*6)], axis=1).shape
+					batch_category_labels = torch.mm(batch_category_labels.float(),P_mat.T) #Shape is 36x8
+					batch_object_labels = torch.mm(batch_object_labels.float(),P_mat.T)
+					#Apply bmm
+					#torch.cuda.empty_cache()
+					P_mat_128 = torch.cat([P_mat.unsqueeze(0)]*128, axis=0).float()
+					#P_mat_128 = P_mat_128.to(torch.device("cuda:1")) #Now shape is 128x8x8
+					#pickle.dump(P_mat_128, open("P_mat_128.p", "wb"))
+					features = features.permute(2, 1, 0) #Now shape is 128 x 8x 36 (used to be 36 x 8x 128)
+					#pickle.dump(features, open("features4.p", "wb"))
+					features = torch.bmm(P_mat_128, features) #shape is 128, 8, 36 
+					features = features.permute(0, 2, 1) #Shape is now 128 x 36 x 8. THIS IS (kind of? reshaped) THE PERMUTED B (B * P^T)
 
-					#PART2
-					#1. Get average features
-						avg_matrix = get_avg_matrix(self.args.permclr_views) #8x2
-						#avg_matrix_128 = torch.zeros(128, self.args.permclr_views*self.args.batch_size, self.args.batch_size).float().to(self.args.device)
-						avg_matrix_128 = torch.cat([avg_matrix.unsqueeze(0)]*128, axis=0).to(self.args.device)#torch.Size([128, 8, 2])
-						features = torch.bmm(features, avg_matrix_128) #This is the average features in Part2-2 #Shape is torch.Size([128, 36, 2])
+				#PART2
+				#1. Get average features
+					avg_matrix = get_avg_matrix(self.args.permclr_views) #8x2
+					#avg_matrix_128 = torch.zeros(128, self.args.permclr_views*self.args.batch_size, self.args.batch_size).float().to(self.args.device)
+					avg_matrix_128 = torch.cat([avg_matrix.unsqueeze(0)]*128, axis=0).to(self.args.device)#torch.Size([128, 8, 2])
+					features = torch.bmm(features, avg_matrix_128) #This is the average features in Part2-2 #Shape is torch.Size([128, 36, 2])
 
-					#2. Get score matrix
-						#Normalize before the elementwise multiplication, for cosine similarity
-						#Normalize across 128
-						features[:, :, 0] = F.normalize(features[:, :, 0].clone(), dim=0); features[:, :, 1] = F.normalize(features[:, :, 1].clone(), dim=0)
-						#Multiple among the dimension of "2"
-						#Shape should be 128 x 36
-						#torch.mul for elementwise multiplication of matrices
-						features = torch.mul(features[:, :, 0].clone(), features[:,:,1].clone()) #Shape is torch.Size([128, 36])
-						#Now sum across the 128 dimensions
-						logits = torch.sum(features, axis=0) #Shape is torch.Size([36])
+				#2. Get score matrix
+					#Normalize before the elementwise multiplication, for cosine similarity
+					#Normalize across 128
+					features[:, :, 0] = F.normalize(features[:, :, 0].clone(), dim=0); features[:, :, 1] = F.normalize(features[:, :, 1].clone(), dim=0)
+					#Multiple among the dimension of "2"
+					#Shape should be 128 x 36
+					#torch.mul for elementwise multiplication of matrices
+					features = torch.mul(features[:, :, 0].clone(), features[:,:,1].clone()) #Shape is torch.Size([128, 36])
+					#Now sum across the 128 dimensions
+					logits = torch.sum(features, axis=0) #Shape is torch.Size([36])
 
-					#3. Put this into NLL loss
-						#Make logits into torch.Size([M x M]) (e.g. 6x6)
-						logits = logits.reshape(M, M)
-						#Positives are the first "batch_size" of each row in the [6x6] above (which has size batch_size * num_classes(M))
-						#copy into (batch_size * M) x M
-						logits = torch.cat([logits.T]*self.args.batch_size, axis=0).T.reshape(2*M, M) #torch.Size([12, 6])
-						#Get labels for logits
-						labels = torch.zeros(logits.shape[0], dtype=torch.long)	
-						labels = put_labels(self.args.batch_size, labels)
-						#Mask logits so that the positives are not counted (e.g. for row 0, 1 is the mask)
-						mask_logits = torch.cat([torch.ones(logits.shape[0], self.args.batch_size), torch.zeros(logits.shape[0], logits.shape[1] - self.args.batch_size)], axis=1).to(self.args.device)	
-						#Code NLL loss with ignore indices
-						logits = logits / self.args.temperature
-						loss = nll(logits, mask_logits, labels)
+				#3. Put this into NLL loss
+					#Make logits into torch.Size([M x M]) (e.g. 6x6)
+					logits = logits.reshape(M, M)
+					#Positives are the first "batch_size" of each row in the [6x6] above (which has size batch_size * num_classes(M))
+					#copy into (batch_size * M) x M
+					logits = torch.cat([logits.T]*self.args.batch_size, axis=0).T.reshape(2*M, M) #torch.Size([12, 6])
+					#Get labels for logits
+					labels = torch.zeros(logits.shape[0], dtype=torch.long)	
+					labels = put_labels(self.args.batch_size, labels)
+					#Mask logits so that the positives are not counted (e.g. for row 0, 1 is the mask)
+					mask_logits = torch.cat([torch.ones(logits.shape[0], self.args.batch_size), torch.zeros(logits.shape[0], logits.shape[1] - self.args.batch_size)], axis=1).to(self.args.device)	
+					#Code NLL loss with ignore indices
+					logits = logits / self.args.temperature
+					loss = nll(logits, mask_logits, labels)
 
-					#Optimizer zero grad
-						self.optimizer.zero_grad()
+				#Optimizer zero grad
+					self.optimizer.zero_grad()
 
-						scaler.scale(loss).backward()
+					scaler.scale(loss).backward()
 
-						scaler.step(self.optimizer)
-						scaler.update()
+					scaler.step(self.optimizer)
+					scaler.update()
 
 				#Schedule
 			if epoch_counter >= 10:
