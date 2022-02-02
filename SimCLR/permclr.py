f5b4b069d52b4826162895ec4bb37440ec008b34
So Yeon Min <soyeonmin@soui-macbookpro.wifi.local.cmu.edu>
2022-02-01 20:03:57 -0500

    run_test_permclr looks fixed.. try again

--- SimCLR/permclr.py
+++ SimCLR/permclr.py
@@ -139,16 +139,15 @@ class PermCLR(object):
 				#Just choose one
 				chosens.append(np.random.choice(c))
 			#chosen training
-			chosen_train_dataset_tuples = [train_datasets[i][chosens[i]] for i in range(len(train_datasets))]
-
+			chosen_train_dataset_tuples = [train_datasets[i][chosens[i]].unsqueeze(0) for i in range(len(train_datasets))] 
 			#catted_img_tups of train dataset
 			train_category_labels_tup =[]
-			for batch_dict in batch_dict_tuple:
+			for batch_dict in chosen_train_dataset_tuples:
 				catted_imgs = torch.cat([batch_dict['image_' + str(i)] for i in range(self.args.permclr_views)]) 
 				train_category_labels_tup.append(catted_imgs) #each catted_image has shape torch.Size([self.args.permclr_views, 3, 32, 32])
-			pickle.dump(batch_dict_tuple, open('batch_dict_tuple.p', 'wb'))
-			pickle.dump(chosen_train_dataset_tuples, open('chosen_train_dataset_tuples.p', 'wb'))
-			pickle.dump(train_category_labels_tup, open('train_category_labels_tup.p', 'wb'))
+			#pickle.dump(batch_dict_tuple, open('batch_dict_tuple.p', 'wb'))
+			#pickle.dump(chosen_train_dataset_tuples, open('chosen_train_dataset_tuples.p', 'wb'))
+			#pickle.dump(train_category_labels_tup, open('train_category_labels_tup.p', 'wb'))
 
 			#catted_img_tups of test dataset
 			catted_imgs_tup = []
