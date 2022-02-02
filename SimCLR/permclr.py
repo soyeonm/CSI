292ed556879787d47b9508644287c6342faaae37
So Yeon Min <soyeonmin@soui-macbookpro.wifi.local.cmu.edu>
2022-02-01 19:59:04 -0500

    save pickle temp

--- SimCLR/permclr.py
+++ SimCLR/permclr.py
@@ -143,11 +143,12 @@ class PermCLR(object):
 
 			#catted_img_tups of train dataset
 			train_category_labels_tup =[]
-			for batch_dict in chosen_train_dataset_tuples:
+			for batch_dict in batch_dict_tuple:
 				catted_imgs = torch.cat([batch_dict['image_' + str(i)] for i in range(self.args.permclr_views)]) 
 				train_category_labels_tup.append(catted_imgs) #each catted_image has shape torch.Size([self.args.permclr_views, 3, 32, 32])
-			#pickle.dump(chosen_train_dataset_tuples, open('chosen_train_dataset_tuples.p', 'wb'))
-			#pickle.dump(train_category_labels_tup, open('train_category_labels_tup.p', 'wb'))
+			pickle.dump(batch_dict_tuple, open('batch_dict_tuple.p', 'wb'))
+			pickle.dump(chosen_train_dataset_tuples, open('chosen_train_dataset_tuples.p', 'wb'))
+			pickle.dump(train_category_labels_tup, open('train_category_labels_tup.p', 'wb'))
 
 			#catted_img_tups of test dataset
 			catted_imgs_tup = []
