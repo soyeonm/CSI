513ecdd682d919c7393254dcd33ccf5880e6f9fa
So Yeon Min <soyeonmin@soui-macbookpro.wifi.local.cmu.edu>
2022-03-07 17:54:35 -0500

    m

--- SimCLR/objclr.py
+++ SimCLR/objclr.py
@@ -137,7 +137,7 @@ class ObjCLR(object):
 					#Now follow the protocol of SupContrast
 					f1, f2 = torch.split(features, [bsz, bsz], dim=0)
 					features = torch.cat([f1.unsqueeze(1), f2.unsqueeze(1)], dim=1) #torch.Size([100, 2, 128])
-					loss = self.sup_con_loss(features, labels)
+					loss = self.sup_con_loss(features)
 					mean_loss += loss.detach().cpu().item()
 					#print("loss is ", loss.detach().cpu().item())
 
