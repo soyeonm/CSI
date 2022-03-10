cf9c1229b2032a3cbb3bec315fa854f74dd97cae
So Yeon Min <soyeonmin@soui-macbookpro.wifi.local.cmu.edu>
2022-03-10 13:21:48 -0500

    m

--- SimCLR/run_objclr.py
+++ SimCLR/run_objclr.py
@@ -21,6 +21,9 @@ import os
 import torch.distributed as dist
 import datetime
 
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
+
 parser.add_argument('--object_views', type=int, default=4)
 #parser.add_argument('--usual_nll', action='store_true')
 parser.add_argument('--model_name', type=str, required=True)
