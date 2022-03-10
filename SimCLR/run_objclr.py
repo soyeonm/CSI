fa0f854a87a5295c5175c1aa48796bb3fb297b10
So Yeon Min <soyeonmin@soui-macbookpro.wifi.local.cmu.edu>
2022-03-10 12:29:13 -0500

    m

--- SimCLR/run_objclr.py
+++ SimCLR/run_objclr.py
@@ -75,8 +75,10 @@ def main_objclr():
 	#train_root_dir = '/home/soyeonm/projects/devendra/CSI/CSI_my/data/co3d_march_9_classify_real/test'
 	train_root_dir = '/home/soyeonm/projects/devendra/CSI/CSI_my/data/co3d_march_9_classify/train'
 	#Add transform later
+	start = time.time()
 	train_dataset = ObjDataset(train_root_dir, args.object_views,  transform=ContrastiveLearningViewGenerator(get_simclr_pipeline_transform(args.co3d_cropsize, 1, args.resize_co3d), 2), processed=False) #transform can be None too
 	#pickle.dump(train_dataset, open("objclr_train_dataset.p", "wb"))
+	print("loaded train dataset in ", (time.time()- start)/60, " mins!")
 	
 	if args.multi_gpu:
 		train_sampler = DistributedSampler(train_dataset, num_replicas=args.n_gpus, rank=args.local_rank)
