e97c8b7064ca1c2f726f303c774ae6fbd7e9ddaf
So Yeon Min <soyeonmin@Soui-MacBookPro.local>
2022-03-10 23:20:29 -0500

    tried refactoring inference

--- SimCLR/run_objclr.py
+++ SimCLR/run_objclr.py
@@ -136,6 +136,7 @@ def main_objclr():
 		pickle.dump(train_class_idx, open("temp_pickles/train_class_idx.p", "wb"))
 
 		test_dataset = ObjInferenceDataset(test_root_dir, args.object_views, resize_shape= args.resize_co3d, shots=None,  transform=None, processed=True, class_idx=train_class_idx)
+		pickle.dump(test_dataset, open("temp_pickles/test_dataset.p", "wb"))
 
 		test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.eval_test_batch_size, num_workers=args.workers, pin_memory=True, shuffle=False)
 		pickle.dump(test_data_loader, open("temp_pickles/test_data_loader.p", "wb"))
