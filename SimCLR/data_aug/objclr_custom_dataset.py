264d8c1547e438b40b705035550b745f06307f85
So Yeon Min <soyeonmin@Soui-MacBookPro.local>
2022-03-15 01:46:12 -0400

    mask crop

--- SimCLR/data_aug/objclr_custom_dataset.py
+++ SimCLR/data_aug/objclr_custom_dataset.py
@@ -166,7 +166,7 @@ class ObjDataset(Dataset):
 				wheres = np.where(mask !=0)
 				start_crop = wheres[0][0]
 				end_crop = wheres[1][0]
-				image = np.asarray(image[start_crop[0]:start_crop[1], end_crop[0]:end_crop[1]])
+				image = np.asarray(image)[start_crop[0]:start_crop[1], end_crop[0]:end_crop[1]]
 				image = Image.fromarray(np.uint8(image))
 			
 			if self.transform is not None:
@@ -328,7 +328,7 @@ class ObjInferenceDataset(Dataset):
 				wheres = np.where(mask !=0)
 				start_crop = wheres[0][0]
 				end_crop = wheres[1][0]
-				image = np.asarray(image[start_crop[0]:start_crop[1], end_crop[0]:end_crop[1]])
+				image = np.asarray(image)[start_crop[0]:start_crop[1], end_crop[0]:end_crop[1]]
 				image = Image.fromarray(np.uint8(image))
 
 			#image = self.resize_transform(image)
